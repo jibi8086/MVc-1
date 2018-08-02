8855d4418840c56ffbd4fff8318a39eac222fce9
jibi8086 <36498166+jibi8086@users.noreply.github.com>
2018-08-02 23:22:32 +0530

    Update LoadDdl.js

--- LoadDdl.js
+++ LoadDdl.js
@@ -29,3 +29,51 @@ LoadDdls('/Package/GetCategoryList', $('#ddlCategory'), 'ID_Package_Category', '
 
 for removing ddl
   $('#ddlConnection').find('option').not(':first').remove();
+
+
+/*
+  public JsonResult GetTaskName()
+        {
+
+            try
+            {
+                string jResult = string.Empty;
+                using (SqlConnection con = new SqlConnection(consString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("VTwo.UspDropDownBind"))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = con;
+                        cmd.Parameters.AddWithValue("@ID_Module", 4);
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader dataReader = cmd.ExecuteReader();
+                        DataTable dataTable = new DataTable();
+                        dataTable.Load(dataReader);
+                        con.Close();
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            jResult = JsonConvert.SerializeObject(dataTable, Newtonsoft.Json.Formatting.Indented);
+                            if (jResult != string.Empty)
+                                return Json(jResult, JsonRequestBehavior.AllowGet);
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                Dispose();
+            }
+            return Json(new { flag = false }, JsonRequestBehavior.AllowGet);
+        }
+        
+        */
